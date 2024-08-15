a129e6abe11e1feafb8b8b28f5270dd803e3a0f8
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2024-08-15 01:44:46 -0500

    move favicon to root of public folder

--- src/config/content/education.tsx
+++ src/config/content/education.tsx
@@ -3,10 +3,16 @@ import { EducationContent } from '../../types/configurable-content';
 const education: EducationContent[] = [
   // UIUC
   {
-    start_date: 'May 2024',
-    end_date: 'Jul 2024',
+    start_date: 'Aug 2022',
+    end_date: 'May 2026',
     school: 'University of Illinois Urbana Champaign',
-    degree: 'Bachelor of Science in Computer Science',
+    degree: (
+      <>
+        Bachelor of Science in Computer Science (Expected May 2025)
+        <br />
+        Professional Master of Computer Science (Expected May 2026)
+      </>
+    ),
     gpa: '3.98/4.00',
     points: [
       <>
