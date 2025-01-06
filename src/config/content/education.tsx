0a50fc1d9d52ac70572e956a591c3132e015eca6
Bennett Wu <57691028+bennettrwu@users.noreply.github.com>
2025-01-06 17:04:30 -0600

    update after finishing fall 2024 semester

--- src/config/content/education.tsx
+++ src/config/content/education.tsx
@@ -7,14 +7,12 @@ const education: EducationContent[] = [
     end_date: 'May 2026',
     school: 'University of Illinois Urbana Champaign',
     degree: 'Combined BS-MCS in Computer Science',
-    gpa: '3.98/4.00',
+    gpa: '3.95/4.00',
     points: [
       <>
         <strong>Coursework:</strong> Algorithms, Artificial Intelligence, Computational Photography, Computer
-        Architecture, Database Systems, Data Structures, Distributed Systems, Parallel Programming
-      </>,
-      <>
-        <strong>In Progress:</strong> Cryptography, Programming Languages and Compilers
+        Architecture, Cloud Computing, Cryptography, Database Systems, Data Structures, Distributed Systems, Parallel
+        Programming
       </>,
     ],
   },
